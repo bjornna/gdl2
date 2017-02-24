bc1895c247d6c775434823f96e267d9576d57115
Manuel Palacio <manuel.palacio@cambio.se>
2017-02-24 16:15:37 +0100

    Update travis ci file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    mvn deploy --settings settings.xml
+    mvn release:clean release:prepare release:perform -B -e --settings settings.xml
 fi

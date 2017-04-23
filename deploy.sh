506b8f421f5789dd2ca2f14bfd6e40506fbf5a88
Manuel Palacio <emanuel.palacio@gmail.com>
2017-04-23 10:43:27 +0200

    Attempt release

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
      mvn release:clean release:prepare release:perform -B -e --settings settings.xml
-     mvn deploy --settings settings.xml
+     #mvn deploy --settings settings.xml
 fi
